23d982e89d5a3ba04687b68c6a3db60295a0234b
Theyvison <theyvison.jf@hotmail.com>
2025-11-11 11:55:29 -0300

    tabalhando com relacionamentos - entidade livro

---
+++ src/test/java/io/github/theyvison/libraryapi/repository/LivroRepositoryTest.java
@@ -0,0 +1,40 @@
+package io.github.theyvison.libraryapi.repository;
+
+import io.github.theyvison.libraryapi.model.Autor;
+import io.github.theyvison.libraryapi.model.GeneroLivro;
+import io.github.theyvison.libraryapi.model.Livro;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.UUID;
+
+@SpringBootTest
+class LivroRepositoryTest {
+    @Autowired
+    LivroRepository livroRepository;
+
+    @Autowired
+    AutorRepository autorRepository;
+
+    @Test
+    void salvarTest() {
+        Livro livro = new Livro();
+        livro.setIsbn("84562-21428");
+        livro.setPreco(BigDecimal.valueOf(100));
+        livro.setGenero(GeneroLivro.FICCAO);
+        livro.setTitulo("UFO");
+        livro.setDataPublicacao(LocalDate.of(1980, 1, 2));
+
+        Autor autor = autorRepository
+                .findById(UUID
+                .fromString("6bd2707e-af0d-49e3-9569-c247f22ceeb8"))
+                .orElse(null);
+
+        livro.setAutor(autor);
+
+        livroRepository.save(livro);
+    }
+}
