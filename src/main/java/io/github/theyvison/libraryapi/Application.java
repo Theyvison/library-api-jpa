bd21a6829f0aa8dd5c0852e9e688b1bbc70d0a7c
Theyvison <theyvison.jf@hotmail.com>
2025-11-11 10:38:49 -0300

    executando uma operação de persistencia com repository

--- src/main/java/io/github/theyvison/libraryapi/Application.java
+++ src/main/java/io/github/theyvison/libraryapi/Application.java
@@ -1,13 +1,29 @@
 package io.github.theyvison.libraryapi;
 
+import io.github.theyvison.libraryapi.model.Autor;
+import io.github.theyvison.libraryapi.repository.AutorRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import java.time.LocalDate;
 
 @SpringBootApplication
 public class Application {
-
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+        var context = SpringApplication.run(Application.class, args);
+        AutorRepository repository = context.getBean(AutorRepository.class);
 
+        exemploSalvarRegistro(repository);
+    }
+
+    public static void exemploSalvarRegistro(AutorRepository autorRepository) {
+        Autor autor = new Autor();
+        autor.setNome("Jose");
+        autor.setNacionalidade("Brasileira");
+        autor.setDataNascimento(LocalDate.of(1950, 1, 31));
+
+        var autorSalvo = autorRepository.save(autor);
+        System.out.println("Autor salvo: " + autorSalvo);
+    }
 }
