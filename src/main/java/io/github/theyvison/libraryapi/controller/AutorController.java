45637e6aec44ffe2816972e3c8806be0396c99bc
Theyvison <theyvison.jf@hotmail.com>
2025-11-18 12:55:38 -0300

    atualização de autores atravÂés do put

--- src/main/java/io/github/theyvison/libraryapi/controller/AutorController.java
+++ src/main/java/io/github/theyvison/libraryapi/controller/AutorController.java
@@ -81,4 +81,23 @@ public class AutorController {
 
         return ResponseEntity.ok(autores);
     }
+
+    @PutMapping("{id}")
+    public ResponseEntity<Void> atualizar(@PathVariable("id") String id, @RequestBody AutorDTO dto) {
+        var idAutor = UUID.fromString(id);
+        Optional<Autor> autorOptional = autorService.obterPorId(idAutor);
+
+        if (autorOptional.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+
+        var autor = autorOptional.get();
+        autor.setNome(dto.nome());
+        autor.setNacionalidade(dto.nacionalidade());
+        autor.setDataNascimento(dto.dataNascimento());
+
+        autorService.atualizar(autor);
+
+        return ResponseEntity.noContent().build();
+    }
 }
