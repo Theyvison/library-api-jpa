31e7ceefe0e19e7aebe5fb689773450f524718b2
Theyvison <theyvison.jf@hotmail.com>
2025-11-20 15:35:36 -0300

    criando a configuração básica do security com securityfilterchain

---
+++ src/main/java/io/github/theyvison/libraryapi/config/SecurityConfiguration.java
@@ -0,0 +1,25 @@
+package io.github.theyvison.libraryapi.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable) // diabilita CSRF para facilitar testes com Postman
+                .formLogin(Customizer.withDefaults()) // habilita form login padrão do Spring Security
+                .httpBasic(Customizer.withDefaults()) // habilita autenticação HTTP Basic
+                .authorizeHttpRequests(authorize ->
+                        authorize.anyRequest().authenticated()) // todas as requisições exigem autenticação
+                .build();
+    }
+}
