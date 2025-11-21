559184f1e8878e0c98c0d6f4c1b31364309b53f4
Theyvison <theyvison.jf@hotmail.com>
2025-11-21 11:14:04 -0300

    implementando o cadastro de usuários

---
+++ src/main/java/io/github/theyvison/libraryapi/service/UsuarioService.java
@@ -0,0 +1,25 @@
+package io.github.theyvison.libraryapi.service;
+
+import io.github.theyvison.libraryapi.model.Usuario;
+import io.github.theyvison.libraryapi.repository.UsuarioRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UsuarioService {
+
+    private final UsuarioRepository usuarioRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public void salvar(Usuario usuario) {
+        var senha = usuario.getSenha();
+        usuario.setSenha(passwordEncoder.encode(senha));
+        usuarioRepository.save(usuario);
+    }
+
+    public Usuario obterPorLogin(String login) {
+        return usuarioRepository.findByLogin(login);
+    }
+}
