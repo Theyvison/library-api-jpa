7329445ffd4bbffb32b8970e18e90640ffc54055
Theyvison <theyvison.jf@hotmail.com>
2025-11-21 11:27:49 -0300

    criando um userdetailsservice customizado

--- src/main/java/io/github/theyvison/libraryapi/config/SecurityConfiguration.java
+++ src/main/java/io/github/theyvison/libraryapi/config/SecurityConfiguration.java
@@ -1,20 +1,19 @@
 package io.github.theyvison.libraryapi.config;
 
 import io.github.theyvison.libraryapi.repository.AutorRepository;
+import io.github.theyvison.libraryapi.security.CustomUserDetailsService;
+import io.github.theyvison.libraryapi.service.UsuarioService;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
@@ -44,8 +43,8 @@ public class SecurityConfiguration {
     }
 
     @Bean
-    public UserDetailsService userDetailsService(PasswordEncoder encoder) {
-        UserDetails user1 = User.builder()
+    public UserDetailsService userDetailsService(UsuarioService usuarioService) {
+        /*UserDetails user1 = User.builder()
                 .username("usuario")
                 .password(encoder.encode("123"))
                 .roles("USER")
@@ -56,7 +55,7 @@ public class SecurityConfiguration {
                 .password(encoder.encode("admin"))
                 .roles("ADMIN")
                 .build();
-
-        return new InMemoryUserDetailsManager(user1, user2);
+        */
+        return new CustomUserDetailsService(usuarioService);
     }
 }
