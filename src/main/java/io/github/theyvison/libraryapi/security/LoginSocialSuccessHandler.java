b9609a11272c92e4bc06c978ace4393f1846a7ae
Theyvison <theyvison.jf@hotmail.com>
2025-11-22 16:58:00 -0300

    lógica para novos usuários que só tem login google

--- src/main/java/io/github/theyvison/libraryapi/security/LoginSocialSuccessHandler.java
+++ src/main/java/io/github/theyvison/libraryapi/security/LoginSocialSuccessHandler.java
@@ -1,23 +1,26 @@
 package io.github.theyvison.libraryapi.security;
 
-import io.github.theyvison.libraryapi.model.Usuario;
-import io.github.theyvison.libraryapi.service.UsuarioService;
+import lombok.RequiredArgsConstructor;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.stereotype.Component;
 import jakarta.servlet.http.HttpServletResponse;
-import lombok.RequiredArgsConstructor;
+import io.github.theyvison.libraryapi.model.Usuario;
 import org.springframework.security.core.Authentication;
+import io.github.theyvison.libraryapi.service.UsuarioService;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
-import org.springframework.security.oauth2.core.user.OAuth2User;
-import org.springframework.stereotype.Component;
 import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
 
 import java.io.IOException;
+import java.util.List;
 
 @Component
 @RequiredArgsConstructor
 public class LoginSocialSuccessHandler extends SavedRequestAwareAuthenticationSuccessHandler {
+
+    private static final String SENHA_PADRAO = "312";
 
     private final UsuarioService usuarioService;
 
@@ -34,10 +37,29 @@ public class LoginSocialSuccessHandler extends SavedRequestAwareAuthenticationSu
 
         Usuario usuario = usuarioService.obterPorEmail(email);
 
+        if (usuario == null) {
+            usuario = cadastrarUsuarioNaBase(email);
+        }
+
         authentication = new CustomAuthentication(usuario);
 
         SecurityContextHolder.getContext().setAuthentication(authentication);
 
         super.onAuthenticationSuccess(request, response, authentication);
+    }
+
+    private Usuario cadastrarUsuarioNaBase(String email) {
+        Usuario usuario;
+        usuario = new Usuario();
+        usuario.setEmail(email);
+        usuario.setLogin(obterLoginAPartirDoEmail(email));
+        usuario.setSenha(SENHA_PADRAO);
+        usuario.setRoles(List.of("OPERADOR"));
+        usuarioService.salvar(usuario);
+        return usuario;
+    }
+
+    private String obterLoginAPartirDoEmail(String email) {
+        return email.substring(0, email.indexOf("@"));
     }
 }
