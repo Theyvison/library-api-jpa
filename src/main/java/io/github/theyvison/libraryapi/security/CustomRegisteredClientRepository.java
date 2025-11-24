8784602f19ce243dd7f2e62edd3ff9a81b6a3d86
Theyvison <theyvison.jf@hotmail.com>
2025-11-24 14:35:27 -0300

    criando o registeredclientsrepository customizado

---
+++ src/main/java/io/github/theyvison/libraryapi/security/CustomRegisteredClientRepository.java
@@ -0,0 +1,46 @@
+package io.github.theyvison.libraryapi.security;
+
+import io.github.theyvison.libraryapi.service.ClientService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class CustomRegisteredClientRepository implements RegisteredClientRepository {
+
+    private ClientService clientService;
+
+    @Override
+    public void save(RegisteredClient registeredClient) {
+
+    }
+
+    @Override
+    public RegisteredClient findById(String id) {
+        return null;
+    }
+
+    @Override
+    public RegisteredClient findByClientId(String clientId) {
+        var client = clientService.obeterPorClientId(clientId);
+
+        if (clientId == null) {
+            return null;
+        }
+
+        return RegisteredClient
+                .withId(client.getId().toString())
+                .clientId(client.getClientId())
+                .clientSecret(client.getClientSecret())
+                .redirectUri(client.getRedirectURI())
+                .scope(client.getScope())
+                .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+                .authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS)
+                .build();
+    }
+}
